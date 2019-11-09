15597f38e7d2fe6915eaff979265013a5837b1ac
NateChris <divineconclusion@gmail.com>
2019-11-09 17:07:44 -0500

    CSS Styling Added

--- app.js
+++ app.js
@@ -8,17 +8,7 @@ window.onload = () => {
     result = document.getElementById("result");
     let btn = document.getElementById("searchBttn");
 
-   btn.onclick = () => {
-    event.preventDefault();
-    const srchParam = document.getElementById("heroName").value;
-    if (srchParam) {
-        query = queryParams + srchParam;
-    } else {
-        query = "";
-    }
-    const endpoint = url + query;
-    //console.log("Endpoint:" + endpoint);
-    fetch(endpoint)
+    fetch(url)
         .then(response => {
             return response.text();
         })
@@ -29,5 +19,27 @@ window.onload = () => {
             //console.log(error);
             result.innerHTML = notFoundError;
         });
-   }
+
+    btn.onclick = () => {
+        event.preventDefault();
+        const srchParam = document.getElementById("heroName").value;
+        if (srchParam) {
+            query = queryParams + srchParam;
+        } else {
+            query = "";
+        }
+        const endpoint = url + query;
+        //console.log("Endpoint:" + endpoint);
+        fetch(endpoint)
+            .then(response => {
+                return response.text();
+            })
+            .then(data => {            
+                result.innerHTML = data;
+            })
+            .catch(error => {
+                //console.log(error);
+                result.innerHTML = notFoundError;
+            });
+    }
 }
